579d87c1b5b8c30cf618828722190f1410ed8485
prog4th <prog4th@proton.me>
2026-01-08 05:35:06 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring04controller/Spring04ControllerApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20260000.spring04controller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring04ControllerApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring04ControllerApplication>(*args)
+}
